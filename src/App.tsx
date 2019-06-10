7506d4b8cb8d0c7fe76d278a1119468012f8117b
Sinan Temel <sinan.temel@nesbilgi.com.tr>
2019-06-10 17:22:14 +0300

    welcome

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 const App: React.FC = () => {
   return (
@@ -10,17 +10,15 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>
+          <h1>WELCOME THE TURKEY</h1>
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
